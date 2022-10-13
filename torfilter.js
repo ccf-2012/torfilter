06ea5843e96f85d97a48d8bd0ebf95eea084e0b1
ccf2012 <crlf66@163.com>
2022-10-13 14:01:06 +0800

    0.7.3

--- torfilter.js
+++ torfilter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         种子列表过滤与认领
 // @namespace    https://greasyfork.org/zh-CN/scripts/451748
-// @version      0.7.2
+// @version      0.7.3
 // @license      GPL-3.0 License
 // @description  在种子列表页中，过滤: 未作种，无国语，有中字，标题不含，描述不含，大小介于，IMDb/豆瓣大于输入值 的种子
 // @author       ccf2012
