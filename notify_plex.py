ea11738e0ba8d928bc7433fade311de80c61966a
ccf2012 <crlf66@163.com>
2023-01-28 00:28:45 +0800

    retry

--- notify_plex.py
+++ notify_plex.py
@@ -3,7 +3,10 @@ from plexapi.server import PlexServer
 from myconfig import CONFIG, readConfig
 import argparse
 import os
+import time
 
+
+MAX_RETRY = 5
 
 def main():
     parser = argparse.ArgumentParser(description='Notify plex server to add a file/folder.')
@@ -30,12 +33,27 @@ def main():
         elif mediaPath.startswith('Movie'):
             lib = plexSrv.library.section('电影')
         else:
+            print("Can't match any library: " + mediaPath)
             return 
 
-    if lib:
-        lib.update(path=os.path.join(CONFIG.plexRootDir, mediaPath))
+        if lib:
+            for n in range(MAX_RETRY):
+                try:    
+                    lib.update(path=os.path.join(CONFIG.plexRootDir, mediaPath))
+                    break
+                except Exception as e:
+                    if n < MAX_RETRY:
+                        print('Fail: lib.update' + str(e))
+                        print('retry %d time.' % (n+1))
+                        time.sleep(3)
+                    else:
+                        print('Error: MAX_RETRY(%d) times' % (MAX_RETRY))
+                        os._exit(1)
+                
+        else:
+            print("Can't find the library section: " + ARGS.library)
     else:
-        print("Can't find the library section: " + ARGS.library)
+        print("Info hash not found/Plex server not connected. ")
 
 
 if __name__ == '__main__':
