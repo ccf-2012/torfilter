110ed8ea012df4129e6161ed4475d607ea072b06
ccf-2012 <crlf66@163.com>
2023-11-30 19:48:09 +0800

    fix dolby

--- torfilter.js
+++ torfilter.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         种子列表过滤
 // @namespace    https://greasyfork.org/zh-CN/scripts/451748
-// @version      1.4.5
+// @version      1.4.6
 // @license      GPL-3.0 License
 // @description  在种子列表页中，过滤: 未作种，无国语，有中字，标题不含，描述不含，大小介于，IMDb/豆瓣大于输入值 的种子。配合dupapi可以实现Plex/Emby库查重。
 // @author       ccf2012
@@ -1839,7 +1839,7 @@ function sleep(ms) {
 function _getDownloadUrlByPossibleHrefs(pagehtml) {
   const possibleHrefs = [
     // pthome
-    "a[href*='downhash'][href*='https'][class!='forward_a']",
+    "a[href*='downhash'][class!='forward_a']",
     // hdchina
     "a[href*='hash'][href*='https'][class!='forward_a']",
     // misc
@@ -1849,7 +1849,7 @@ function _getDownloadUrlByPossibleHrefs(pagehtml) {
   ];
 
 
-  dllink = null;
+  var dllink = null;
   for (const href of possibleHrefs) {
     const query = $(href, pagehtml);
     if (query.length) {
